fff9fbde4c31dc1aa3ff1c10251ff3b83f3af7f6
mohamed <harrademed@gmail.com>
2023-06-15 11:03:28 -0700

    fixed again 4th script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
  * main - primary function
- * return : 0 if exited properly
- * puts :  print message to the screen
+ * return - 0 if exited properly
+ * puts -  print message to the screen
  */
 int main(void)
 {
