c023a78949d3b4f5d682f5394f6179cc1bb2cb24
mohamed <harrademed@gmail.com>
2023-06-15 11:06:18 -0700

    fixed again 4th script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - primary function
- * return - 0 if exited properly
- * puts -  print message to the screen
+ * Return: 0 if exited properly
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+    puts("\"Programming is like building a multilingual puzzle\"");
+    return (0);
 }
